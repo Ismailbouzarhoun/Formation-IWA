a709ac98c80f9b51471cce2d55e636db4c6ad991
Ismail Bouzarhoun <Ismailbouzarhoune09@gmail.com>
2023-11-14 16:38:44 +0100

    TP3 JAVASCRIPT

---
+++ Javascript/TP3/TP3.js
@@ -0,0 +1,8 @@
+function addVille(){
+    const id = document.getElementById('id').value
+    const nom = document.getElementById('nom').value
+    const prenom = document.getElementById('prenom').value
+    const filiere = document.getElementById('filiere').value
+
+    let student = document.getElementById('students').innerHTML
+}
