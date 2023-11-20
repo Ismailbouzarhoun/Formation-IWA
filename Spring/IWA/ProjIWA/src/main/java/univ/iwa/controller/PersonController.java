dab8d0a1255c39720eeaea76991aa9c17aa1a1b4
Ismail Bouzarhoun <Ismailbouzarhoune09@gmail.com>
2023-11-20 16:57:41 +0100

    Update PersonController.java

--- Spring/IWA/ProjIWA/src/main/java/univ/iwa/controller/PersonController.java
+++ Spring/IWA/ProjIWA/src/main/java/univ/iwa/controller/PersonController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import univ.iwa.model.Person;
 import univ.iwa.repository.PersonRepository;
-
 @RestController
 public class PersonController {
 	@Autowired PersonRepository personRepository;
